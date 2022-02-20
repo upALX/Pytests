a21458bc6df57876585b102c4d80e5b7cbe2243e
Jose Alexandre <alexandreaddres@gmail.com>
2022-02-20 07:39:07 -0300

    adding a private atributes

--- app.py
+++ app.py
@@ -1,18 +1,45 @@
-# class Account:
-#     def __init__(self, user_value, user_name,user_balance, user_limit):
-#         self.account = user_value
-#         self.titular = user_name
-#         self.balance = user_balance
-#         self.limit = user_limit
-
-class Sistema():
-    def __init__(self, text_user):
-        self.text = text_user
+class Account:
+    def __init__(self, user_value, user_name,user_balance, user_limit):
+        self.__account = user_value
+        self.__titular = user_name
+        self.__balance = user_balance
+        self.__limit = user_limit
 
 
-    def le_entrada(self):
-        self.texto = input('Digite a entrada: ')
+    def extrato(self):
+        print('Seu saldo é de {:.f3}.'.format(self.__balance))
 
 
-    def exibe_saida(self):
-        print('O texto digitado é: {}'.format(self.texto))
+    def deposita(self, value_dep):
+        self.__balance += value_dep
+        print('Seu saldo ficou em {:.f3}'.format(self.__balance))
+
+
+    def saque(self, value_saq):
+        self.__balance -= value_saq
+        print('Seu saldo ficou em {:.f3}'.format(self.__balance))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Sistema():
+#     def __init__(self):
+#         self.text = ''
+#
+#
+#     def le_entrada(self):
+#         self.texto = input('Digite a entrada: ')
+#
+#
+#     def exibe_saida(self):
+#         print('O texto digitado é: {}'.format(self.texto))
