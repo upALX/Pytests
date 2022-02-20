7fdb9cf4f2a0a3c9724d8a107bd1d57750390470
Jose Alexandre <alexandreaddres@gmail.com>
2022-02-20 04:23:47 -0300

    tests

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
-def create_account(number_id, name_user, balance_value):
-    account = {"id_user": number_id, "user_name": name_user, "balance": balance_value}
-    return account
+# class Account:
+#     def __init__(self, user_value, user_name,user_balance, user_limit):
+#         self.account = user_value
+#         self.titular = user_name
+#         self.balance = user_balance
+#         self.limit = user_limit
+
+class Sistema():
+    def __init__(self, text_user):
+        self.text = text_user
 
 
-account = create_account(1, "Alexandre", "10.000")
-
-print('Sua conta é a numero {}, você é o corretista {} e seu saldo em conta é {}'.format(account["id_user"],
-                                                                                         account["user_name"],
-                                                                                         account["balance"]))
+    def le_entrada(self):
+        self.texto = input('Digite a entrada: ')
 
 
-def deposit_value_in_account(account, value_push):
-    account["balance"] += value_push
-
-
-def pull_money_to_the_account(account, pull_money):
-    account["balance"] -= pull_money
-
-
-def show_balance(account):
-    print(account["balance"])
+    def exibe_saida(self):
+        print('O texto digitado é: {}'.format(self.texto))
